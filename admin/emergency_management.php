36341835ba1117f306aa2bb53424bf0c592eab47
MUHAMMAD NAJIB <psychokillerspree@gmail.com>
2026-07-15 22:39:37 +0800

    Fix missing db connection causing crash on emergency management page

--- admin/emergency_management.php
+++ admin/emergency_management.php
@@ -5,6 +5,7 @@ include '../config/error.php';
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
+include '../config/db.php';
 include '../config/session.php';
 
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'admin') {
